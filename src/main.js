d6b07934c51c0d1f1d3ae694fea5b2d78ed0f261
Bhavya Anil <banil@ucsc.edu>
2025-02-08 22:00:34 -0800

    set up phaser

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// name: Bhavya Anil
+// game: INCLUDE TITLE
+// hours: 10 mins
+// creative tilt: WRITE JUSTIFICATION
+
+let config = {
+    type: Phaser.AUTO,
+    width: 640, 
+    height: 480,
+    scene: [Menu, Play]
+}
+
+let game = new Phaser.Game(config);
