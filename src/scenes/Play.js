64d4c1f67ee299bda527c5978777bee54ce8f92b
Bhavya Anil <banil@ucsc.edu>
2025-02-10 00:19:30 -0800

    edited platforms

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,11 +8,11 @@ class Play extends Phaser.Scene{
         this.background = this.add.tileSprite(0, 0, 640, 740, 'background').setOrigin(0,0);
         
         //platform
-        const platforms = this.physics.add.staticGroup();
+        this.platforms = this.physics.add.staticGroup();
         for (let i = 0; i < 5; i++){
             const x = Phaser.Math.Between(150, 400);
             const y = 150 * i;
-            const platform = platforms.create(x, y, 'rainbow');
+            const platform = this.platforms.create(x, y, 'rainbow');
             platform.scale = 0.5
             const body = platform.body;
             body.updateFromGameObject();
@@ -20,7 +20,7 @@ class Play extends Phaser.Scene{
        
        //dog
        this.dogSprite = this.physics.add.sprite(240, 320, 'dog-left').setScale(2);
-       this.physics.add.collider(platforms, this.dogSprite);
+       this.physics.add.collider(this.platforms, this.dogSprite);
 
        this.dogSprite.body.checkCollision.up = false;
        this.dogSprite.body.checkCollision.left = false;
@@ -35,6 +35,17 @@ class Play extends Phaser.Scene{
     update(){
         //moving  background
         this.background.tilePositionX -= 0.2;
+
+        //platform
+        this.platforms.children.iterate(child => {
+            const platform = child;
+            platform.y += 1.5;
+            if (platform.y >= 740){
+                platform.y = Phaser.Math.Between(-100, -50);
+                platform.x = Phaser.Math.Between(150, 400);
+                platform.body.updateFromGameObject();
+            }
+        });
 
         //jumping mechanism
         const touchDown = this.dogSprite.body.touching.down;
